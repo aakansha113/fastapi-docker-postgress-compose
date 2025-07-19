e917f611c2a64f6355a0f201393e527d211abf2d
Aakansha <aakanshahujare@gmail.com>
2025-07-19 09:23:00 +0000

    Initials Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI  # ✅ Correct class name and import
+
+app = FastAPI()  # ✅ Now instantiate it here
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from FastAPI inside Docker!"}
+
+@app.get("/hello/")
+def hello():
+    return{"message":"hii iam am hello"}
+
+@app.get("/welcome/")
+def welcome():
+    return{"message":"hello welcome"}
